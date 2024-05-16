0e69a98439281962c2215d380cf5eef36338b258
Simon Nestler <simon.nestler@thi.de>
2024-05-16 10:50:49 +0200

    Methoden fuer Componenten

--- src/Klausur.java
+++ src/Klausur.java
@@ -1,3 +1,6 @@
+import java.awt.Color;
+import java.awt.Font;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
@@ -10,9 +13,45 @@ public class Klausur {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JLabel label = new JLabel("Vorname");
+
         label.setBounds(50, 20, 200, 30);
         frame.add(label);
 
         frame.setVisible(true);
+    }
+
+    private void methodenFuerKomponenten() {
+        JLabel label = new JLabel("Vorname");
+
+        // Ermittlung der Hintergrundfarbe der Komponente label
+        Color hintergrundfarbe = label.getBackground();
+
+        // Setzen der Hintergrundfarbe der Komponente label auf rot
+        label.setBackground(Color.RED);
+
+        // Setzen der Hintergrundfarbe auf blau
+        Color blau = new Color(2, 2, 255);
+        Color blau2 = new Color(0.01f, 0.1f, 1f);
+
+        label.setBackground(blau);
+
+        // Setzen der Vordergrundfarbe
+        label.setForeground(Color.GREEN);
+
+        // Auslesen der Vordergrundfarbe
+        Color vordergrundfarbe = label.getForeground();
+
+        // Auslesen der Font
+        Font font = label.getFont();
+
+        // Setzen der Schriftart
+        Font arial20 = new Font("Arial", Font.BOLD | Font.ITALIC, 20);
+        label.setFont(arial20);
+
+        // Auslesen der Höhe
+        int hoehe = label.getHeight();
+
+        // Auslesen der Breite
+        int breite = label.getWidth();
     }
 }
