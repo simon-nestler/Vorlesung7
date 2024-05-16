2eed8dd208ddc52130520211981779ced0e19c4e
Simon Nestler <simon.nestler@thi.de>
2024-05-16 11:48:06 +0200

    Farbe des Textes nun veranderbar

--- src/Klausur.java
+++ src/Klausur.java
@@ -1,6 +1,7 @@
 import java.awt.Color;
 import java.awt.Font;
 
+import javax.swing.JColorChooser;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -9,23 +10,25 @@ import javax.swing.JTextField;
 public class Klausur {
 
     public Klausur() {
-        /*
-         * JFrame frame = new JFrame("Klausur");
-         * frame.setSize(400, 300);
-         * frame.setBackground(Color.BLUE);
-         * frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-         * 
-         * JLabel label = new JLabel("Vorname");
-         * 
-         * label.setBounds(50, 20, 200, 30);
-         * label.setForeground(Color.RED);
-         * label.setBackground(Color.YELLOW);
-         * label.setFont(new Font("Verdana", Font.BOLD, 20));
-         * frame.add(label);
-         * 
-         * frame.setVisible(true);
-         */
-        modalDialog();
+        JFrame frame = new JFrame("Klausur");
+        frame.setSize(400, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel label = new JLabel("Vorname");
+
+        label.setBounds(50, 20, 200, 30);
+        label.setFont(new Font("Verdana", Font.BOLD, 20));
+        frame.add(label);
+
+        frame.setVisible(true);
+        label.setForeground(colorChooser(frame));
+
+        // modalDialog();
+
+    }
+
+    private Color colorChooser(JFrame frame) {
+        return JColorChooser.showDialog(frame, "Farbauswahl", Color.RED);
     }
 
     private void modalDialog() {
